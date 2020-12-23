5318b338219a5531d5a61f5877bb61f94462cf40
Abel Vazquez <abel.vm@inspide.com>
2020-12-23 16:53:35 +0100

    home cleaning

--- examples/script.js
+++ examples/script.js
@@ -1,47 +1,46 @@
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 
 import '../maplibre/mapbox-gl.js';
 import '../dist/h3t.js';
 
 const
-  lib = globalThis.maplibre || globalThis.mapboxgl,
-  map = new lib.Map({
-    container: 'map',
-    style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
-    center: [-3.703793, 40.416687],
-    zoom: 16,
-    minZoom: 14,
-    maxZoom: 21
-  });
+  lib = globalThis.maplibre || globalThis.mapboxgl;
+const map = new lib.Map({
+  container: 'map',
+  style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
+  center: [-3.703793, 40.416687],
+  zoom: 16,
+  minZoom: 14,
+  maxZoom: 21,
+});
 
 map.on('load', () => {
-
   map.addH3Source({
-    'map': map,
-    'h3field': 'h3_id',
-    'https': true,
-    'sourcename': 'test-source',
-    'sourcelayer': 'mapillary-images',
-    'sourceoptions': {
-      'type': 'vector',
+    map,
+    h3field: 'h3_id',
+    https: true,
+    sourcename: 'test-source',
+    sourcelayer: 'mapillary-images',
+    sourceoptions: {
+      type: 'vector',
       // 'tiles': ['h3t://portall-api.inspide.com/v0/indicators/h3/{z}/{x}/{y}.mvt'],
-      'tiles': ['h3t://tiles3.mapillary.com/v0.1/{z}/{x}/{y}.mvt'],
-      'format': 'pbf',
-      'minzoom': 14,
-      'maxzoom': 14
+      tiles: ['h3t://tiles3.mapillary.com/v0.1/{z}/{x}/{y}.mvt'],
+      format: 'pbf',
+      minzoom: 14,
+      maxzoom: 14,
     },
-    'debug': true
+    debug: true,
   });
 
   map.addLayer({
-    'id': 'test-layer',
-    'type': 'fill',
-    'source': 'test-source',
+    id: 'test-layer',
+    type: 'fill',
+    source: 'test-source',
     'source-layer': 'mapillary-images',
-    'paint':  {
+    paint: {
       'fill-color': '#f00',
-      'fill-opacity': 0.25
-    }
+      'fill-opacity': 0.25,
+    },
   });
 });
 
--- index.js
+++ index.js
@@ -1,56 +1,55 @@
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 const
-    lib = globalThis.maplibre || globalThis.mapboxgl,
-    utils = {
-        'tovt': require('geojson-vt'),
-        'togeojson': require('@mapbox/vt2geojson'),
-        'topbf': require('vt-pbf'),
-        'h3': require('h3-js')
-    },
-    h3tsource = o => {
-        lib.addProtocol('h3t', (params, callback) => {
-            const
-                t = performance.now(),
-                u = `http${(o.https === false)?'':'s'}://${params.url.split('://')[1]}`,
-                h = o.h3field || 'h3_id',
-                s = params.url.split(/\/|\./i),
-                l = s.length,
-                zxy = s.slice(l - 4, l - 1).map(s => s * 1);
-            utils.togeojson({
-                uri: u,
-                layer: o.sourcelayer
-            }, function (e, gj) {
-                if (e) throw e;
-                if (gj.features.length === 0) {
-                    callback(null, null, null, null);
-                    !!o.debug && console.warn(`${u}: Empty`);
-                } else {
-                    gj.features = gj.features.map(f => {
-                        //TODO: todo lo de h3
-                         const
-                            c = f.geometry.coordinates, 
-                            h = utils.h3.geoToH3(c[1], c[0], 12);  
-                        f.id = parseInt(h, 16);
-                        f.properties.h3 = h;
-                        f.geometry.type = 'Polygon';
-                        f.geometry.coordinates = [utils.h3.h3ToGeoBoundary(h, true)];
-                        return f;
-                    }); 
-                    const
-                        f = utils.tovt(gj).getTile(...zxy),
-                        fo = {};
-                    fo[o.sourcelayer] = f;
-                    const
-                        p = utils.topbf.fromGeojsonVt(
-                            fo,
-                            { 'version': 2 }
-                        );
-                    callback(null, p, null, null);
-                    !!o.debug && console.log(`${u}: ${gj.features.length} features, ${(performance.now() - t).toFixed(0)} ms`);
-                }
-            });
+  lib = globalThis.maplibre || globalThis.mapboxgl;
+const utils = {
+  tovt: require('geojson-vt'),
+  togeojson: require('@mapbox/vt2geojson'),
+  topbf: require('vt-pbf'),
+  h3: require('h3-js'),
+};
+const h3tsource = (o) => {
+  lib.addProtocol('h3t', (params, callback) => {
+    const t = performance.now();
+    const u = `http${(o.https === false) ? '' : 's'}://${params.url.split('://')[1]}`;
+    // const h3 = o.h3field || 'h3_id';
+    const s = params.url.split(/\/|\./i);
+    const l = s.length;
+    const zxy = s.slice(l - 4, l - 1).map((k) => k * 1);
+    utils.togeojson({
+      uri: u,
+      layer: o.sourcelayer,
+    }, (e, gj) => {
+      if (e) throw e;
+      if (gj.features.length === 0) {
+        callback(null, null, null, null);
+        !!o.debug && console.warn(`${u}: Empty`);
+      } else {
+        gj.features = gj.features.map((f) => {
+          // TODO: todo lo de h3
+          const
+            c = f.geometry.coordinates;
+          const h = utils.h3.geoToH3(c[1], c[0], 12);
+          f.id = parseInt(h, 16);
+          f.properties.h3 = h;
+          f.geometry.type = 'Polygon';
+          f.geometry.coordinates = [utils.h3.h3ToGeoBoundary(h, true)];
+          return f;
         });
-        o.map.addSource(o.sourcename, o.sourceoptions);
-    };
+        const
+          f = utils.tovt(gj).getTile(...zxy);
+        const fo = {};
+        fo[o.sourcelayer] = f;
+        const
+          p = utils.topbf.fromGeojsonVt(
+            fo,
+            { version: 2 }
+          );
+        callback(null, p, null, null);
+        !!o.debug && console.log(`${u}: ${gj.features.length} features, ${(performance.now() - t).toFixed(0)} ms`);
+      }
+    });
+  });
+  o.map.addSource(o.sourcename, o.sourceoptions);
+};
 
 lib.Map.prototype.addH3Source = h3tsource;
