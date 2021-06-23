1b0b9a295fe58d01bf85848c6a26bde6392dfe13
Abel Vazquez <abel.vm@inspide.com>
2021-06-23 20:49:03 +0200

    add geojson source and setData method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 /* jshint esversion: 9 */
 
-/*
-
-  TODO:
-
-  * ...
-
-*/
-
 const lib = globalThis.maplibregl;
 const utils = {
   tovt: require('geojson-vt'),
@@ -18,42 +10,40 @@ const defaults ={
   "geometry_type": 'Polygon',
   "timeout": 0,
   "debug": false,
-  "newline": '\n', 
-  "separator": ',',
   "promoteId": true,
-  "type": 'vector',
-  "format": 'pbf',
   "https": true
 };
-const csv2json = (csv, newline, separator, header) => {
-  const lines = csv.split(newline);
-  let h, ls, k;
-  if (header == undefined) {
-    [h, ...ls] = lines;
-    k = h.split(separator);
-  } else {
-    k = header;
-    ls = lines;
-  }
-  return (lines.length === 1 && lines[0] === '') ? new Array(0) : ls.map(l => (v =>
-    k.reduce(
-      (c, n, i) => ({
-        ...c,
-        [n]: v[i],
-      }),
-      {}
-    )
-  )(l.split(separator)));
+// json to geojson
+const j2g = (js, o) => {
+  const fs = js.cells.map(j => {
+    const feature = {
+      "properties": j,
+      "geometry": {
+        "type": o.geometry_type,
+        "coordinates": o.generate(j.h3_id)
+      }
+    };
+    if(!!!o.promoteID) feature.id = parseInt(j.h3_id, 16);
+    return feature;
+  });
+  return { "type": "FeatureCollection", "features": fs };
 };
 const tileparser = (tile, options) => {
   return new Promise((resolve, reject) => {
-    resolve(csv2json(tile, options.newline, options.separator, options.header));
+    resolve(j2g(tile, options));
   });
 };
+
+/*
+
+  Custom vector tiles source
+
+*/
 const h3tsource = (name, options) => {
-  const o = Object.assign({}, defaults, options);
+  const o = Object.assign({}, defaults, options, {"type": 'vector', "format": 'pbf'});
   o.generate = (o.geometry_type === 'Polygon') ? h3id => [utils.h3.h3ToGeoBoundary(h3id, true)] : h3id => utils.h3.h3ToGeo(h3id).reverse();
   if(!!o.promoteId) o.promoteId = o.h3field;
+  
   lib.addProtocol('h3t', (params, callback) => {
     const t = performance.now();
     const u = `http${(o.https === false) ? '' : 's'}://${params.url.split('://')[1]}`;
@@ -73,21 +63,9 @@ const h3tsource = (name, options) => {
           throw new Error(r.statusText);
         }
       })
-      .then(t => t.text())
-      .then(t => tparser(t, o))
-      .then(js => {
-        const fs = js.map(j => {
-          const feature = {
-            "properties": j,
-            "geometry": {
-              "type": o.geometry_type,
-              "coordinates": o.generate(j.h3_id)
-            }
-          };
-          if(!!!o.promoteID) feature.id = parseInt(j.h3_id, 16);
-          return feature;
-        });
-        const g = { "type": "FeatureCollection", "features": fs };
+      .then(t => t.json())
+      .then(js => tileparser(js))
+      .then(g => {
         const f = utils.tovt(g).getTile(...zxy);
         const fo = {};
         fo[o.sourcelayer] = f;
@@ -108,5 +86,82 @@ const h3tsource = (name, options) => {
   //o.map.addSource(o.sourcename, o.sourceoptions);
   this.addSource(name, o);
 };
-
 lib.Map.prototype.addH3TSource = h3tsource;
+
+
+/*
+
+  Custom geojson source
+
+*/
+const h3jsource = (name, options) => {
+  const t = performance.now();
+  const controller = new AbortController();
+  const signal = controller.signal;
+  const o = Object.assign({}, defaults, options, {"type": 'geojson'});
+  o.generate = (o.geometry_type === 'Polygon') ? h3id => [utils.h3.h3ToGeoBoundary(h3id, true)] : h3id => utils.h3.h3ToGeo(h3id).reverse();
+  if(!!o.promoteId) o.promoteId = o.h3field;
+  if (o.timeout > 0) setTimeout(() => controller.abort(), o.timeout);
+  fetch(u, {signal})
+      .then(r => {
+        if (r.ok) {
+          return r.text();
+        } else {
+          throw new Error(r.statusText);
+        }
+      })
+      .then(t => t.json())
+      .then(js => tileparser(js))
+      .then(g => {
+        o.data = g;
+        this.addSource(name, o);
+        if (!!o.debug) console.log(`${u}: ${g.features.length} features, ${(performance.now() - t).toFixed(0)} ms`);
+      })
+      .catch(e => {
+        if (e.name === 'AbortError') e.message = `Timeout: Tile ${u} is taking too long to fetch`;
+        console.error(e.message);
+      });  
+};
+lib.Map.prototype.addH3JSource = h3jsource;
+
+
+/*
+
+  Update data in geojson/H3J layer
+
+*/
+const h3jsetdata = (name, data) => {
+  const t = performance.now();
+  const controller = new AbortController();
+  const signal = controller.signal;
+  const s = this.getSource(name);
+  const o = s._options;
+  //const o = Object.assign({}, defaults, options, {"type": 'geojson'});
+  //o.generate = (o.geometry_type === 'Polygon') ? h3id => [utils.h3.h3ToGeoBoundary(h3id, true)] : h3id => utils.h3.h3ToGeo(h3id).reverse();
+  //if(!!o.promoteId) o.promoteId = o.h3field;
+  if(typeof data === 'string'){
+    if (o.timeout > 0) setTimeout(() => controller.abort(), o.timeout);
+    fetch(u, {signal})
+      .then(r => {
+        if (r.ok) {
+          return r.text();
+        } else {
+          throw new Error(r.statusText);
+        }
+      })
+      .then(t => t.json())
+      .then(js => tileparser(js))
+      .then(g => {
+        this.getSource(name).setData(g);
+        if (!!o.debug) console.log(`${u}: ${g.features.length} features, ${(performance.now() - t).toFixed(0)} ms`);
+      })
+      .catch(e => {
+        if (e.name === 'AbortError') e.message = `Timeout: Tile ${u} is taking too long to fetch`;
+        console.error(e.message);
+      });
+  }else{
+    tileparser(data)
+    .then(g => this.getSource(name).setData(g));
+  }
+};
+lib.Map.prototype.setH3JData = h3jsetdata;
